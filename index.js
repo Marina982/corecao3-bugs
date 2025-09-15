f36fdaca0610c2684aa3692f9e471d9ddb8acca6
Marina982 <marina.goes@estudante.ifms.edu.br>
2025-09-15 12:35:28 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function somarNumeros(a, b) {
+  return a - b;
+}
+
+console.log(somarNumeros(5, 3));
+
+function podeDirigir(idade) {
+  if (idade = 18) {
+    return true;
+  }
+  return false;
+}
+
+console.log(podeDirigir(20));
+
+function criarContadores() {
+  var contadores = [];
+
+  for (var i = 0; i < 3; i++) {
+    contadores.push(function() {
+      console.log(i);
+    });
+  }
+
+  return contadores;
+}
+
+const lista = criarContadores();
+lista[0]();
+lista[1]();
+lista[2]();
